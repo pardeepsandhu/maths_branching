57f5b6b90a1e9d7fb7879c08b16109209dcd38dc
Pardeep Sandhu <P14163846@GWHXX682LIN011W.lec-admin.dmu.ac.uk>
2016-02-16 17:33:53 +0000

    Trying to add this file to github

---
+++ math_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+float add (float x, float y)
+{
+    return x + y;
+}
+
+float sub ()
+{
+    return x - y;
+}
+
+float mul()
+{
+	return x*y;
+}
+
+float div()
+{
+	return x/y;
+}
+
+int main()
+{
+    float a, b, r;
+    char op;
+    do {
+       printf("number  op  number?  ");
+       scanf(" %f %c %f", &a, &op, &b);
+       switch (op)
+       {
+           case '+' : r = add(a,b);
+                      break;
+	   case '-' : r = sub(a,b);
+		      break;
+	   case '*' : r = mul(a,b);
+		      break;
+	   case '/' : r = div(a,b);
+		      break;
+           default  : op='?';
+       }
+       if (op=='?')
+          printf("Unknown operator\n");
+       else if (op=='q')
+          printf("Bye\n");
+       else
+          printf("%f %c %f = %f\n", a, op, b, r);
+    }
+    while (op != 'q');
+    
+    return 0;
+}
